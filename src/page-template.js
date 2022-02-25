dbc554d91fed05237fe0e14ba95d7259431cafa2
pcion <patrick.ciongoli@gmail.com>
2022-02-25 18:54:59 -0500

    fixed html content

--- src/page-template.js
+++ src/page-template.js
@@ -3,11 +3,11 @@ const generatePage = (name, github) => {
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8
+            <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>Portfolio Demo</title>
-        <head>
+        </head>
 
         <body>
             <h1>${name}</h1>
